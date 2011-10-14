13a45b92d581d1b60a89d777a8586cb9c00887ba
Jeremy McCarthy <jeremy@Jeremys-MacBook-Pro.local>
2011-10-14 01:19:27 -0700

    Initial Commit

---
+++ cp120hw1/cp120hw1AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  cp120hw1AppDelegate.h
+//  cp120hw1
+//
+//  Created by Jeremy McCarthy on 10/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface cp120hw1AppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ cp120hw1/cp120hw1AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  cp120hw1AppDelegate.m
+//  cp120hw1
+//
+//  Created by Jeremy McCarthy on 10/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "cp120hw1AppDelegate.h"
+
+@implementation cp120hw1AppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
