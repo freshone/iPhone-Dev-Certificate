33649937e36ba8f5e3332da7a6eda9db993baae9
Jeremy McCarthy <jeremy@Jeremys-MacBook-Pro.local>
2011-10-17 02:21:44 -0700

    First major night of progress on hw1

--- cp120hw1/cp120hw1AppDelegate.h
+++ cp120hw1/cp120hw1AppDelegate.h
@@ -10,8 +10,35 @@
 
 @interface cp120hw1AppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow *window;
+    NSButton *helloButton;
+    NSButton *goodbyeButton;
+    NSTextField *helloLabel;
+    NSButton *copyButton;
+    NSTextField *copyTextField;
+    NSSegmentedControl *numberSegmentedControl;
+    NSTextField *numberLabel;
+    NSMatrix *seasonRadioButtons;
+    NSTextField *seasonLabel;
+    NSButton *nowButton;
+    NSTextField *nowLabel;
 }
 
 @property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSButton *helloButton;
+@property (assign) IBOutlet NSButton *goodbyeButton;
+@property (assign) IBOutlet NSButton *copyButton;
+@property (assign) IBOutlet NSTextField *helloLabel;
+@property (assign) IBOutlet NSTextField *copyTextField;
+@property (assign) IBOutlet NSSegmentedControl *numberSegmentedControl;
+@property (assign) IBOutlet NSTextField *numberLabel;
+@property (assign) IBOutlet NSMatrix *seasonRadioButtons;
+@property (assign) IBOutlet NSTextField *seasonLabel;
+@property (assign) IBOutlet NSButton *nowButton;
+@property (assign) IBOutlet NSTextField *nowLabel;
+
+- (IBAction)changeHelloLabel:(id)sender;
+- (IBAction)changeNumberLabel:(id)sender;
+- (IBAction)changeSeasonLabel:(id)sender;
+- (IBAction)changeNowLabel:(id)sender;
 
 @end
--- cp120hw1/cp120hw1AppDelegate.m
+++ cp120hw1/cp120hw1AppDelegate.m
@@ -11,10 +11,83 @@
 @implementation cp120hw1AppDelegate
 
 @synthesize window;
+@synthesize helloButton;
+@synthesize goodbyeButton;
+@synthesize helloLabel;
+@synthesize copyButton;
+@synthesize copyTextField;
+@synthesize numberSegmentedControl;
+@synthesize numberLabel;
+@synthesize seasonRadioButtons;
+@synthesize seasonLabel;
+@synthesize nowButton;
+@synthesize nowLabel;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    [ self changeHelloLabel:helloButton ];
+    [ self changeNumberLabel:numberSegmentedControl ];
+    [ self changeSeasonLabel:seasonRadioButtons ];
+    [ self changeNowLabel:nowButton ];
 }
 
+- (IBAction)changeHelloLabel:(id)sender {
+    NSString *labelString;
+    
+    if( [ sender isEqualTo:helloButton ] ) {
+        labelString = @"Hello World!";
+    }
+    else if( [ sender isEqualTo:goodbyeButton ] ) {
+        labelString = @"Goodbye";
+    }
+    else if( [ sender isEqual:copyButton ] ) {
+        labelString = [ copyTextField stringValue ];
+    }
+    else {
+        labelString = @"Error";
+    }
+
+    [ helloLabel setStringValue:labelString ];
+}
+
+- (IBAction)changeNumberLabel:(id)sender {
+    NSInteger index = [ numberSegmentedControl selectedSegment ];
+    
+    if( index == 0 ) {
+        [ numberLabel setStringValue:@"0:Zero" ];
+    }
+    else if( index == 1 ) {
+        [ numberLabel setStringValue:@"1:One" ];
+    }
+    else if( index == 2 ) {
+        [ numberLabel setStringValue:@"2:Two" ];
+    }
+}
+
+- (IBAction)changeSeasonLabel:(id)sender {
+    NSString *seasonString = [ [ seasonRadioButtons selectedCell ] title ];
+    
+    if( [ seasonString isEqualToString:@"Winter" ] ) {
+        [ seasonLabel setStringValue:@"December" ];
+    }
+    else if( [ seasonString isEqualToString:@"Spring" ] ) {
+        [ seasonLabel setStringValue:@"March" ];
+    }
+    else if( [ seasonString isEqualToString:@"Summer" ] ) {
+        [ seasonLabel setStringValue:@"June" ];
+    }
+    else if( [ seasonString isEqualToString:@"Fall" ] ) {
+        [ seasonLabel setStringValue:@"September"];
+    }
+}
+
+- (IBAction)changeNowLabel:(id)sender {
+    NSDate *nowDate = [ NSDate date ];
+    NSDateFormatter *dateFormatter = [ [ NSDateFormatter alloc ] init ];
+    [ dateFormatter setTimeStyle:NSDateFormatterLongStyle ];
+    [ dateFormatter setDateStyle:NSDateFormatterLongStyle ];
+    [ nowLabel setStringValue:[ dateFormatter stringFromDate:nowDate ] ];
+    [ dateFormatter release ];
+}
+    
 @end
