503780f330a112a30d09947acfac68878f1a0bd4
Jeremy McCarthy <jeremy@d-173-250-145-44.dhcp4.washington.edu>
2011-10-24 19:52:57 -0700

    Initial Commit

---
+++ cp120hw2/cp120hw2AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  cp120hw2AppDelegate.m
+//  cp120hw2
+//
+//  Created by Jeremy McCarthy on 10/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "cp120hw2AppDelegate.h"
+
+@implementation cp120hw2AppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ cp120hw2/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  cp120hw2
+//
+//  Created by Jeremy McCarthy on 10/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
