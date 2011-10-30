1bdbac0a1c23feff9607b5d52334ce9ad2244d1d
Jeremy McCarthy <jeremy@Jeremys-MacBook-Pro.local>
2011-10-29 22:47:03 -0700

    Initial Commit

---
+++ cp120hw3/MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  cp120hw3
+//
+//  Created by Jeremy McCarthy on 10/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument {
+}
+
+@end
