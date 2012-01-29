5f415198e2d228c0fe6485d87d1ccd7afc9edf3c
Jeremy McCarthy <jeremy@Jeremys-MacBook-Pro.local>
2012-01-28 23:00:50 -0800

    Initial Commit

---
+++ BobbleHead/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BobbleHead
+//
+//  Created by Jeremy McCarthy on 1/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BobbleHeadTests/BobbleHeadTests.h
@@ -0,0 +1,13 @@
+//
+//  BobbleHeadTests.h
+//  BobbleHeadTests
+//
+//  Created by Jeremy McCarthy on 1/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BobbleHeadTests : SenTestCase
+
+@end
