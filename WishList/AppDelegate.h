db24136e9fd9d4e0e947be93566395769dd4675d
Jeremy McCarthy <jeremy@Jeremys-MacBook-Pro.local>
2012-02-04 19:23:14 -0800

    Initial Commit

---
+++ WishList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WishList
+//
+//  Created by Jeremy McCarthy on 2/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ WishList/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  WishList
+//
+//  Created by Jeremy McCarthy on 2/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
