d462eeb0461cd7120cad50c034103204a856c314
Jeremy McCarthy <jeremy@Jeremys-MacBook-Pro.local>
2012-02-28 22:09:57 -0800

    Initial Commit

---
+++ PathMap/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PathMap
+//
+//  Created by Jeremy McCarthy on 2/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PathMap/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PathMap
+//
+//  Created by Jeremy McCarthy on 2/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
