f2d2393394b2948f53a588c6e1e4732462994c11
Jeremy D. McCarthy <mccarthy.jeremy@gmail.com>
2012-03-21 01:34:14 -0700

    cleaned up and committed

--- BobbleHead/ViewController.m
+++ BobbleHead/ViewController.m
@@ -9,6 +9,37 @@
 #import "ViewController.h"
 
 @implementation ViewController
+@synthesize headImageView;
+@synthesize headSegmentedControl;
+
+static CGFloat UPDATE_INTERVAL = 1.0f/60; 
+
+- (IBAction)changeHeadImage:(id)sender
+{
+    NSInteger index = [headSegmentedControl selectedSegmentIndex];
+    NSInteger headCount = [headImages count];
+    
+    if(index >= 0 && index < headCount)
+        [headImageView setImage:[headImages objectAtIndex:index]];    
+}
+
+- (void)updateHeadPostion:(NSTimer *)aTimer {
+    CMAcceleration acceleration = motionManager.accelerometerData.acceleration;
+    NSLog(@"x = %f | y = %f | z = %f\n", acceleration.x, acceleration.y, acceleration.z);
+    
+    // reset head to the origin
+    headPosition = headOriginPosition;
+    
+    // calculate the new position
+    headPosition.x += roundf(acceleration.x * 150);
+    headPosition.y += roundf(-acceleration.y * 150);
+    
+    // set a lower bound on y axis
+    headPosition.y = fminf(headPosition.y, headOriginPosition.y + 20.0);
+    
+    // draw
+    headImageView.center = headPosition;
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -21,14 +52,45 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    // initialize images
+    dominicHeadImage = [UIImage imageNamed:@"dominic.png"];
+    harrisonHeadImage = [UIImage imageNamed:@"harrison.png"];
+    headImages = [[NSArray arrayWithObjects:dominicHeadImage, harrisonHeadImage, nil] retain];
+    [self changeHeadImage:headSegmentedControl];
+    headOriginPosition = headPosition = headImageView.center;
+    
+    // initialize motion
+    motionManager = [[[CMMotionManager alloc] init] retain];
+    motionManager.accelerometerUpdateInterval = UPDATE_INTERVAL;
+    [motionManager startAccelerometerUpdates];
+    
+    // initialize simulation timer
+    timer = [[NSTimer scheduledTimerWithTimeInterval:UPDATE_INTERVAL
+                                              target:self
+                                            selector:@selector(updateHeadPostion:)
+                                            userInfo:nil
+                                             repeats:YES] retain];
 }
 
 - (void)viewDidUnload
 {
+    [self setHeadImageView:nil];
+    [self setHeadSegmentedControl:nil];
+    [headImages release];
+    [timer release];
+    
+    // turn off motion
+    [motionManager stopAccelerometerUpdates];
+    [motionManager release];
+    motionManager = nil;
+    
+    // invalidate and release the timer
+    [timer invalidate];
+    [timer release];
+    timer = nil;
+    
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -53,12 +115,11 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
+    // don't ever rotate, just keep bobblin'
+    return NO;
+    
     // Return YES for supported orientations
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-    } else {
-        return YES;
-    }
+    //return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
 @end
