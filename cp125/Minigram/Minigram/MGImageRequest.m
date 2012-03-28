6a6e29730b69aeea461d53b6dc845ad5de7832ef
Jeremy D. McCarthy <mccarthy.jeremy@gmail.com>
2012-03-28 03:06:03 -0700

    Thumbnails look nice now

--- cp125/Minigram/Minigram/MGImageRequest.m
+++ cp125/Minigram/Minigram/MGImageRequest.m
@@ -15,18 +15,6 @@
 #pragma mark -
 #pragma mark Constant Declarations
 
-static NSString * const BASE_URL = @"http://minigram.herokuapp.com";
-static NSString * const PHOTOS_URL = @"/photos";
-static NSString * const API_KEY = @"73edf8a0-3fb7-012f-5dee-48bcc8c61670";
-static NSString * const API_KEY_FIELD = @"X-Minigram-Auth";
-static NSString * const FORMAT = @"application/json";
-static NSString * const FORMAT_FIELD = @"Accept";
-static NSString * const CONTENT_TYPE = @"content-type";
-static NSString * const PHOTO_TITLE_FIELD = @"photo[title]";
-static NSString * const PHOTO_IMAGE_FIELD = @"photo[image]";
-static NSString * const PHOTO_MIME_TYPE = @"image/jpeg";
-static NSString * const PHOTO_FILENAME = @"no-filename.jpg";
-
 - (void)send
 {
     [[self photo] setImage:nil];
--- cp125/Minigram/Minigram/MGThumbnailRequest.m
+++ cp125/Minigram/Minigram/MGThumbnailRequest.m
@@ -16,18 +16,6 @@
 #pragma mark -
 #pragma mark Constant Declarations
 
-static NSString * const BASE_URL = @"http://minigram.herokuapp.com";
-static NSString * const PHOTOS_URL = @"/photos";
-static NSString * const API_KEY = @"73edf8a0-3fb7-012f-5dee-48bcc8c61670";
-static NSString * const API_KEY_FIELD = @"X-Minigram-Auth";
-static NSString * const FORMAT = @"application/json";
-static NSString * const FORMAT_FIELD = @"Accept";
-static NSString * const CONTENT_TYPE = @"content-type";
-static NSString * const PHOTO_TITLE_FIELD = @"photo[title]";
-static NSString * const PHOTO_IMAGE_FIELD = @"photo[image]";
-static NSString * const PHOTO_MIME_TYPE = @"image/jpeg";
-static NSString * const PHOTO_FILENAME = @"no-filename.jpg";
-
 - (void)send
 {
     [[self photo] setThumbnail:nil];
@@ -39,7 +27,21 @@ static NSString * const PHOTO_FILENAME = @"no-filename.jpg";
 
 - (void)connectionDidFinishLoading:(NSURLConnection*)connection
 {
-    [[self photo] setThumbnail:[UIImage imageWithData:[self responseData]]];
+    UIImage *image = [UIImage imageWithData:[self responseData]];
+    UIImage *placeholder = [UIImage imageNamed:@"Placeholder.png"];
+    
+    // Make a square thumbnail out of this badboy
+    if (image.size.width != placeholder.size.width && image.size.height != placeholder.size.height)
+	{
+        CGSize itemSize = CGSizeMake(placeholder.size.width, placeholder.size.height);
+		UIGraphicsBeginImageContext(itemSize);
+		CGRect imageRect = CGRectMake(0.0, 0.0, itemSize.width, itemSize.height);
+		[image drawInRect:imageRect];
+		image = UIGraphicsGetImageFromCurrentImageContext();
+		UIGraphicsEndImageContext();
+    }
+    
+    [[self photo] setThumbnail:image];
     [[self delegate] requestDidComplete:self];
 }
 
