aa645a73f496ca16ea13b3f888771ff66129cb33
Jeremy D. McCarthy <mccarthy.jeremy@gmail.com>
2012-04-10 02:02:14 -0700

    SliderPuzzle is moving main view instead of subviews

---
+++ cp125/SliderPuzzle/SliderPuzzle/PuzzleTile.h
@@ -0,0 +1,13 @@
+//
+//  PuzzleTile.h
+//  SliderPuzzle
+//
+//  Created by Jeremy McCarthy on 4/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PuzzleTile : NSObject
+
+@end
