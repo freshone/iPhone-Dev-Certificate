8ffd6279bf1b219df86c132332739a919918aa09
Jeremy D. McCarthy <mccarthy.jeremy@gmail.com>
2012-06-10 23:22:16 -0700

    Adding new DesktopServer project

---
+++ cp130/HW3/DesktopServer/DesktopServer/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  DesktopServer
+//
+//  Created by Jeremy McCarthy on 6/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ cp130/HW3/DesktopServer/DesktopServer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DesktopServer
+//
+//  Created by Jeremy McCarthy on 6/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
