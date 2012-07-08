0ef6729abba266cf4e3ee56d1e9ba0767ede4a62
Jeremy D. McCarthy <mccarthy.jeremy@gmail.com>
2012-07-08 02:01:54 -0700

    Basic skeleton complete. Saving before I break stuff.

---
+++ cp130/HW7/HW7/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HW7
+//
+//  Created by Jeremy McCarthy on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
